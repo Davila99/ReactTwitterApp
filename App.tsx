2fbccc90e1475d6b335be51d58a2ec1d905bfbfb
Davila99 <davilaeliseo453@gmail.com>
2021-11-15 18:54:42 -0600

    Creando la aplicacion twitter utilizando React Navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,29 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+const HomeScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
